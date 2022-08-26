2936b47633445b6aa03164df94d038c141bccd55
Mitchell Foote <mcfoote321@gmail.com>
2022-08-26 16:06:51 -0400

    Add comments & todos & fix site linking.

---
+++ assets/js/deck-builder.js
@@ -0,0 +1,5 @@
+//TODO import function to handle local storage data from index search.
+
+//TODO recycle search functions from pricing js and modals from index js if needed.
+
+//TODO deck-builder page functionality.
