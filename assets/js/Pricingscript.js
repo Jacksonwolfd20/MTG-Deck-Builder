d183f86c306db1e88a61e7c43f8961b0bc92b6cd
Mitchell Foote <mcfoote321@gmail.com>
2022-09-01 00:15:51 -0400

    add forex api, and edit spelling, remove test func

--- assets/js/Pricingscript.js
+++ assets/js/Pricingscript.js
@@ -24,13 +24,8 @@ localStorage.setItem('deck', JSON.stringify(Deck))
 
 // Pulls from local storage to display the first card
 function firstSearch() {
-  //localStorage.getItem('indexSearch', searchInput);
-  //localStorage.getItem('indexUsdCheck', usdCheck);
-  //localStorage.getItem('indexEurCheck', eurCheck);
 
   var searchinput = localStorage.getItem('indexSearch')
-
-  //console.log(localStorage.getItem('indexSearch', searchInput));
 
   cardInput(searchinput);
 }
@@ -54,7 +49,6 @@ searchbtn.addEventListener("click", function () {
   event.preventDefault();
   // Get the card entereed
   var searchinput = $("#search-input").val().trim();
-  console.log(searchinput);
   //Verify a Card Name was entered
   if (searchinput === "" || searchinput == "undefined") {
     modal.classList.add('is-active');
@@ -62,8 +56,6 @@ searchbtn.addEventListener("click", function () {
   } else {
     // Switches the spaces with + symbol
     searchinput = additionSymbolAdd(searchinput);
-    //Test what is going to the api
-    console.log(searchinput);
     //Gets card shop info
     cardInput(searchinput);
     //Checks only one checkmark to determine the value of the other
@@ -76,23 +68,7 @@ searchbtn.addEventListener("click", function () {
     }
   }
 
-
-}
-);
-
-
-//coding card img finder
-function cardImgcreator(cardarray) {
-  //Stores card Name
-  var cardRealName = cardarray.name
-  //Stores the img to be used later on
-  var cardImgTest = cardarray.image_uris.border_crop
-  //Starts To Get The Price
-  console.log(cardRealName);
-  //Test To see if the ID is called
-  console.log(cardImgTest);
-}
-
+});
 
 //Gets card shop info
 function cardInput(searchinput) {
@@ -113,11 +89,9 @@ function cardInput(searchinput) {
       var cardarray = data
       //Checks the card name
       var cardRealName = cardarray.name
-      //Check the card id
-      var cardmarketid = cardarray.cardmarket_id
       //see if there is actully a card under the name
       if (!cardarray.name) {
-        console.log('No results found!');
+
         resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
       } else {
         resultContentEl.textContent = '';
@@ -130,12 +104,6 @@ function cardInput(searchinput) {
 
         //Adds Showing of Results 
         resultTextEl.textContent = (" " + cardRealName);
-        //Sends Cards Real Name
-        console.log(cardRealName);
-        //Test To see if the ID is called
-        console.log(cardmarketid);
-        //img creator
-        cardImgcreator(cardarray);
 
       }
     })
@@ -146,53 +114,51 @@ function getCardPrice(cardarray) {
   //Gathers the prices from the API  
   cardPriceUsd = cardarray.prices.usd
   cardPriceUsdFoil = cardarray.prices.usd_foil
-  cardPriceEuro = cardarray.prices.eur
-  cardPriceEuroFoil = cardarray.prices.eur_foil
-  //testing and pplying definitions to be used later
+
+  var exchangeQuery = encodeURI(`https://www.freeforexapi.com/api/live?pairs=USDEUR`);
+
+  fetch(exchangeQuery, {
+    method: 'GET', //GET is the default.
+    credentials: 'same-origin', // include, *same-origin, omit
+    redirect: 'follow', // manual, *follow, error
+    cache: 'reload'  // Refresh the cache
+  })
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+  var exchangeRate = exchangeQuery.data.rate
+
+  cardPriceEuro = cardPriceUsd * exchangeRate;
+  cardPriceEuroFoil = cardPriceUsdFoil * exchangeRate;
+  
+  //testing and applying definitions to be used later
   if (cardPriceUsd === null && cardPriceUsdFoil === null) {
-    console.log("Normal card price is unavalible");
-    console.log("Foil card price is unavalible");
-    displayPriceUsd = ("Normal card price is unavalible")
-    displayPriceUsdFoil = ("Foil card price is unavalible")
+    displayPriceUsd = ("Normal card price is unavailable")
+    displayPriceUsdFoil = ("Foil card price is unavailable")
   } else if (cardPriceUsd && cardPriceUsdFoil === null) {
-    console.log(" $ " + cardPriceUsd + "~");
-    console.log("Foil card price is unavalible");
     displayPriceUsd = (" $ " + cardPriceUsd + "~")
-    displayPriceUsdFoil = ("Foil card price is unavalible")
+    displayPriceUsdFoil = ("Foil card price is unavailable")
   } else if (cardPriceUsd === null && cardPriceUsdFoil) {
-    console.log("Normal card price is unavalible");
-    console.log(" $ " + cardPriceUsdFoil + "~");
-    displayPriceUsd = ("Normal card price is unavalible")
+    displayPriceUsd = ("Normal card price is unavailable")
     displayPriceUsdFoil = (" $ " + cardPriceUsdFoil + "~")
   } else {
-    console.log(" $ " + cardPriceUsd + "~");
-    console.log(" $ " + cardPriceUsdFoil + "~");
     displayPriceUsd = (" $ " + cardPriceUsd + "~")
     displayPriceUsdFoil = (" $ " + cardPriceUsdFoil + "~")
   }
 
   if (cardPriceEuro === null && cardPriceEuroFoil === null) {
-    console.log("Normal card price is unavalible");
-    console.log("Foil card price is unavalible");
-    displayPriceEuro = ("Normal card price is unavalible")
-    displayPriceEuroFoil = ("Foil card price is unavalible")
+    displayPriceEuro = ("Normal card price is unavailable")
+    displayPriceEuroFoil = ("Foil card price is unavailable")
   } else if (cardPriceEuro && cardPriceEuroFoil === null) {
-    console.log(" € " + cardPriceEuro + "~");
-    console.log("Foil card price is unavalible");
     displayPriceEuro = (" € " + cardPriceEuro + "~")
-    displayPriceEuroFoil = ("Foil card price is unavalible")
+    displayPriceEuroFoil = ("Foil card price is unavailable")
   } else if (cardPriceEuro === null && cardPriceEuroFoil) {
-    console.log("Normal card price is unavalible");
-    console.log("Foil card price is € " + cardPriceEuroFoil + "~");
-    displayPriceEuro = ("Normal card price is unavalible")
+    displayPriceEuro = ("Normal card price is unavailable")
     displayPriceEuroFoil = (" € " + cardPriceEuroFoil + "~")
   } else {
-    console.log("Card price is € " + cardPriceEuro + "~");
-    console.log("Foil card price is € " + cardPriceEuroFoil + "~");
     displayPriceEuro = (" € " + cardPriceEuro + "~")
     displayPriceEuroFoil = (" € " + cardPriceEuroFoil + "~")
   }
-
 
 };
 
@@ -213,8 +179,6 @@ function printCards(cardarray) {
 
   titleEl.textContent = cardarray.name;
 
-
-
   var bodyContentEl = document.createElement('p');
   bodyContentEl.innerHTML =
     '<strong>Card Type:</strong> ' + cardarray.type_line + '<br/>';
@@ -225,9 +189,9 @@ function printCards(cardarray) {
       '<strong>Card Foil Price:</strong> ' + displayPriceUsdFoil + '<br/>';
   } else {
     bodyContentEl.innerHTML +=
-      '<strong>Card Price:</strong> ' + displayPriceEuro + '<br/>';
+      '<strong>Card Price:</strong> ' + displayPriceEuro +  '<a href="https://www.freeforexapi.com"><img alt="Free Forex API" src="https://www.freeforexapi.com/Images/link.png" height="20"> </a>' + '<br/>';
     bodyContentEl.innerHTML +=
-      '<strong>Card Foil Price:</strong> ' + displayPriceEuroFoil + '<br/>';
+      '<strong>Card Foil Price:</strong> ' + displayPriceEuroFoil + +  '<a href="https://www.freeforexapi.com"><img alt="Free Forex API" src="https://www.freeforexapi.com/Images/link.png" height="20"> </a>' + '<br/>';
   }
 
   if (cardarray.oracle_text) {
@@ -258,14 +222,12 @@ function printCards(cardarray) {
     var obj = JSON.parse(localStorage.getItem('deck'));
 
     if (obj.length > 99) {
-      console.log("To Big")
       modal.classList.add('is-active');
     } else if (obj.includes(cardarray.name) ) {
       if (cardarray.type_line.startsWith("Basic Land") || cardarray.type_line.startsWith("Basic Snow Land") || cardarray.name.startsWith("Dragon's Approach" || cardarray.name.startsWith("Persistent Petitioners") || cardarray.name.startsWith("Rat Colony")|| cardarray.name.startsWith("Relentless Rats") || cardarray.name.startsWith("Shadowborn Apostle"))){
         Deck.push(cardarray.name);//Add the text 'item1' to Deck
         localStorage.setItem('deck', JSON.stringify(Deck))
         obj = JSON.parse(localStorage.getItem('deck'));
-        console.log(obj)
       }else{
         modal.classList.add('is-active');
       }
@@ -273,7 +235,6 @@ function printCards(cardarray) {
       Deck.push(cardarray.name);//Add the text 'item1' to Deck
       localStorage.setItem('deck', JSON.stringify(Deck))
       obj = JSON.parse(localStorage.getItem('deck'));
-      console.log(obj)
     }
 
   })
@@ -300,7 +261,6 @@ function allCardNames() {
       return response.json();
     })
     .then(data => {
-      console.log(data);
       //Stores the data to be used later on
       cardNamesAuto = data.data
       $('#search-input').autocomplete({
@@ -318,18 +278,4 @@ function allCardNames() {
   return;
 }
 
-
-
-
 allCardNames();
-
-
-
-
-
-
-
-
-
-
-
