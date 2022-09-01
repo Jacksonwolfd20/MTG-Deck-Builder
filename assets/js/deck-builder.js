28b94a87266070badd4b84fcf6c351f84fb8716c
zdedm <zdedmonson@gmail.com>
2022-08-31 22:42:15 -0400

    image and pricing functionality

--- assets/js/deck-builder.js
+++ assets/js/deck-builder.js
@@ -1,18 +1,13 @@
-var searchBox = document.querySelector('#search-box');
-var searchBtn = document.querySelector('#search-button');
-var searchBar = document.querySelector('#search-bar');
-
-var cardDisplay = document.querySelector('#card-display');
-
 var usdBtn = document.querySelector('#usd-button');
 var eurBtn = document.querySelector('#eur-button');
 var standardBtn = document.querySelector('#standard-button');
 var foilBtn = document.querySelector('#foil-button');
 
+var cardDisplay = document.querySelector('#card-display');
 var cardCost = document.querySelector('#card-cost');
-var addBtn = document.querySelector('#add-button');
 
 var deckList = document.querySelector('#deck-list');
+var clearbtn = document.querySelector("#clearButton");
 
 var newDeck = [];
 
@@ -65,27 +60,6 @@ foilBtn.addEventListener('click', function() {
     }
 });
 
-
-// search button functionality
-searchBtn.addEventListener('click', function() {
-    event.preventDefault();
-    responseEl.remove();
-
-    // grabs the user's input from the search box
-    var searchInput = searchBar.val().trim();
-    console.log(searchInput);
-    
-    // checks for a valid input
-    if (searchInput === '' || searchInput == 'undefined') {
-        console.log('No results found');
-        var responseEl = document.createElement('p');
-        responseEl.textContent = "Sorry, the card either couldn't be found or it doesn't exist. Please try again!"
-        searchBox.append(responseEl);
-    } else {
-        updateDisplay(searchInput);
-    }
-});
-
 // when the page is opened, function checks for an existing deck and prints it to the page if it exists
 function retrieveDeck() {
     // checks if localStorage 'deck' exists
@@ -95,7 +69,6 @@ function retrieveDeck() {
 
     // stores localStorage 'deck' in a variable and sorts it alphabetically
     let oldDeck = JSON.parse(localStorage.getItem('deck'));
-    oldDeck.sort();
     console.log(oldDeck);
     
     // loops once for each item in oldDeck
@@ -103,90 +76,112 @@ function retrieveDeck() {
         // creates new <li> with the name of the card at oldDeck[i]
         var node = document.createTextNode("");
         node = oldDeck[i];
+
         var listItem = document.createElement("li");
-        
+        listItem.classList.add('label', 'deletetext' + [i]);
+        listItem.setAttribute("id", "Card" + [i]);
+
+        var countCards = [i]
+
         // prints card to the page
         listItem.append(node);
-        deckList.append(listItem);
+
+        // creates a button to remove the card
+        var removeButtonEl = document.createElement('button');
+        removeButtonEl.textContent = 'Remove Card';
+        removeButtonEl.classList.add('button', 'deleteButton' + [i]);
+        deckList.append(listItem, removeButtonEl);
+
+        var cardButtonSinglei = document.querySelector('.deleteButton' + [i]);
+        console.log("button" + [i])
+        var cardtextSinglei = document.querySelector('.deletetext' + [i]);
+        console.log("text" + [i])
+
+        cardButtonSinglei.addEventListener("click", function () {
+            cardButtonSinglei.remove('button');
+            console.log("button" + [i])
+            cardtextSinglei.textContent = ' ';
+            console.log("text" + [i])
+         })
+    }
+ 
+    for (var i = 0; i < countCards; i++) {
+        
+        var cardButtonSinglei = document.querySelector('.deleteButton' + [i]);
+        console.log("button" + [i])
+        var cardtextSinglei = document.querySelector('.deletetext' + [i]);
+        console.log("text" + [i])
+
+        cardButtonSinglei.addEventListener("click", function () {
+            cardButtonSinglei.remove('button');
+            console.log("button" + [i])
+            cardtextSinglei.textContent = ' ';
+            console.log("text" + [i])
+         })
     }
 
-    console.log(localStorage.getItem('deck'));
-};
+    var cardButtonSingle = document.querySelector('.deleteButton' + countCards);
+    var cardtextSingle = document.querySelector('.deletetext' + countCards);
 
-/*
-// checks for user input and returns card image to the page
-function updateDisplay(searchInput) {
-    //
-    var cardId = encodeURI(`https://api.scryfall.com/cards/named?fuzzy=${searchInput}`);
+    cardButtonSingle.addEventListener("click", function () {
+        cardButtonSingle.remove('button');
+        cardtextSingle.textContent = ' ';
+    })
+
+    clearbtn.addEventListener("click", function () { 
+        var Deck = []
+        localStorage.setItem('deck', JSON.stringify(Deck))
+        console.log(localStorage.getItem('deck'))
+        for (var i = 0; i < countCards; i++) {
+        var cardText = document.querySelector('.deletetext' + [i]);
+        document.querySelector('.deleteButton' + [i]).innerHTML = ' ';
+        cardText.textContent = ' ';
+        var ButtonText = document.querySelector('.deleteButton' + [i]);
+        ButtonText.remove('button');
+        }
+        cardText = document.querySelector('.deletetext' + countCards);
+        document.querySelector('.deleteButton' + countCards).innerHTML = ' ';
+        cardText.textContent = ' ';
+        ButtonText = document.querySelector('.deleteButton' + countCards);
+        ButtonText.remove('button');
+        console.log(localStorage.getItem('deck'));
+    }
+)};
+
+deckList.querySelector('li').addEventListener('click', function() {
+    var cardName = this.textContent;
+    var cardId = encodeURI(`https://api.scryfall.com/cards/named?fuzzy=${cardName}`);
 
     fetch(cardId, {
         method: 'GET',
         credentials: 'same-origin',
         redirect: 'follow',
         cache: 'reload'
+        
     }) .then (response => {
         return response.json();
-    }) .then (data => {
-        var cardArray = data;
-        var cardName = cardArray.name;
-        var marketId = cardArray.cardmarket_id
 
-        if (!cardName) {
-            console.log('No results found');
-            var responseEl = document.createElement('p');
-            responseEl.textContent = "Sorry, the card either couldn't be found or it doesn't exist. Please try again!"
-            searchBox.append(responseEl);
-        } else {
-            getCard(cardArray);
+    }) .then (data => {
+        var price = "";
+
+        if(usdBtn.classList.contains("is-selected") && standardBtn.classList.contains("is-selected")) {
+            price = data.prices.usd;
+        } else if(usdBtn.classList.contains("is-selected") && foilBtn.classList.contains("is-selected")) {
+            price = data.prices.usd_foil;
+        } else if(eurBtn.classList.contains("is-selected") && standardBtn.classList.contains("is-selected")) {
+            price = data.prices.eur;
+        } else if(eurBtn.classList.contains("is-selected") && foilBtn.classList.contains("is-selected")) {
+            price = data.prices.eur_foil;
         }
+
+        cardDisplay.src = selected.image_uris.border_crop;
+        cardCost.textContent = price
+
     }) .catch(error => {
         console.error('Error:', error);
+
     })
     return;
-};
-
-function getCard(cardArray) {
-    var cardImage = cardArray.image_uris.border_crop;
-    cardDisplay.src = cardImage;
-
-    var cardPriceUsd = cardArray.prices.usd;
-    var cardPriceUsdFoil = cardArray.prices.usd_foil;
-    var cardPriceEuro = cardArray.prices.eur;
-    var cardPriceEuroFoil = cardArray.prices.eur_foil;
-
-    //if (
-
-};
-
-function allCardNames() {
-    //
-    var cardIdName = encodeURI(`https://api.scryfall.com/catalog/card-names`);
-  
-    fetch(cardIdName, {
-      method: 'GET',
-      credentials: 'same-origin',
-      redirect: 'follow',
-      cache: 'reload'
-    }) .then (response => {
-        return response.json();
-    }) .then (data => {
-        console.log(data);
-
-        cardNameAuto = data.data
-
-        searchBar.autocomplete({
-        maxResults: 10,
-        source: function(request, response) {
-            var results = $.ui.autocomplete.filter(cardNameAuto, request.term);
-            response(results.slice(0, 10));
-            }
-        });
-    }) .catch (error => {
-        console.error('Error:', error);
-    });
-    return;
-};
-*/
+});
 
 retrieveDeck();
-//allCardNames();
